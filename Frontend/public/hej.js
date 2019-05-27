b52aecfbe00b84394e2414be8164d48417dc3a19
saidkayed <36620432+saidkayed@users.noreply.github.com>
2019-05-28 00:46:19 +0200

    d

---
+++ Frontend/public/hej.js
@@ -0,0 +1,6 @@
+
+
+
+
+
+console.log("fuck dig")
--- Frontend/public/service-worker-custom.js
+++ Frontend/public/service-worker-custom.js
@@ -1,37 +1,50 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+//de sider der skal cashes.
+const filesToCache = [
+
+];
+
+const staticCacheName = 'pages-cache-v1';
+
+//installere Service Worker
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+//Når du er offline vil den modtage fetch kald, og vil prøve at finde den caschede side, hvis den er gemt.
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
 
 
-if(workbox){
-console.log("My custom service worker")
-}else{
-    console.log("My custom workbox didnt load")
-}
 
-workbox.routing.registerRoute(
-    new RegExp('.*\.js'),
-    new workbox.strategies.NetworkFirst()
-);
+      // de sider du besøger bliver cashet. eller videre sender den dig videre til en 404 side/offline side
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+      .then(response => {
 
-
-workbox.routing.registerRoute(
-    "http://localhost:3001",
-    workbox.strategies.networkFirst(),
-);
+          return caches.open(staticCacheName).then(cache => {
+            cache.put(event.request.url, response.clone());
+            console.log("added");
+            return response;
+          });
+        });
 
 
-workbox.routing.registerRoute(
-    "http://localhost:3001/#/",
-    workbox.strategies.networkFirst(),
-);
-
-workbox.routing.registerRoute(
-    "http://localhost:3001/#/create",
-    workbox.strategies.networkFirst(),
-);
-
-workbox.routing.registerRoute(
-    "http://localhost:3001/#/login",
-    workbox.strategies.networkFirst(),
-);
-
-
+    }).catch(error => {
+       console.log("ERROR",error);
+    })
+  );
+});
